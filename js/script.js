20223ebc95d8d617daa756a2c6a024c65ee97bf3
denis343434 <161150159+denis343434@users.noreply.github.com>
2024-05-01 14:56:25 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -4,3 +4,15 @@ $('.active').click(function(){
 $('.close').click(function(){
     $('.menu').toggleClass('d-n');
 });
+$('.coment-active').click(function(){
+    $('.fon-blur').toggleClass('d-n');
+});
+$('.coment-close').click(function(){
+    $('.fon-blur').toggleClass('d-n');
+});
+$('.active01').click(function(){
+    $('.menu-contacts').toggleClass('d-n');
+});
+$('.close-menu-contacts').click(function(){
+    $('.menu-contacts').toggleClass('d-n');
+});
